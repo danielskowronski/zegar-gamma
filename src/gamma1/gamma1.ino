82d50ba2ebcbd8c9f008571b13e560ad7a08d96b
Daniel Skowroński <daniel@dsinf.net>
2016-07-20 22:19:53 +0200

    adds DS18B20 external thm probe, sets up second line display for 2 thm zones - build 0015

--- src/gamma1/gamma1.ino
+++ src/gamma1/gamma1.ino
@@ -4,7 +4,9 @@
 #include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
 #include <DS18B20.h>
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
+  
 #define I2C_ADDR          0x27 
 #define BACKLIGHT_PIN     3
 #define En_pin  2
@@ -16,24 +18,29 @@
 #define D7_pin  7
 
 #define thm_int_pin 7
-#define thm_ext_pin 0
+#define ONE_WIRE_BUS 5
 
 LiquidCrystal_I2C	lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 RTC_DS1307 rtc;
 char monthsNames[12][4] = { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" };
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);  
+uint8_t numDevices;
+
 // http://www.quinapalus.com/hd44780udg.html
 byte arrowUpRight[8] = {0,15,3,5,9,16,0}, arrowDownRight[8] = {0,16,9,5,3,15,0}, degreeCels[8] = {24,24,3,4,4,4,3}, internalSymbol[8] = {31,17,21,14,31,4,4}, externalSymbol[8]={4,14,31,4,21,17,31};
 
 void setup() {  
+  sensors.begin();
   
   lcd.begin (16,2);
   lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
   lcd.setBacklight(HIGH);
   
   lcd.createChar(0, arrowUpRight);
-  lcd.createChar(1, arrowDownRight);
+  lcd.createChar(1, arrowDownRight);  
   lcd.createChar(2, degreeCels);
   lcd.createChar(3, internalSymbol);
   lcd.createChar(4, externalSymbol);
@@ -41,7 +48,7 @@ void setup() {
   lcd.home ();
   lcd.print("Zegar Gamma [ds]");  
   lcd.setCursor(0,1);
-  lcd.print("build 0007");
+  lcd.print("build 0015");
   
   for (int i=5; i>=0; i--){
     lcd.setCursor(15,1);lcd.print(i);
--- src/gamma1/ui.ino
+++ src/gamma1/ui.ino
@@ -32,7 +32,7 @@ int incmod(int x, int mod){
 }
 
 int currIdx=0;
-int trend(int* array, int start=currIdx, int count=10){
+int trend(int* array, int start=currIdx, int count=60){
   int i=start;
   int balance=0;
   do {
@@ -41,10 +41,11 @@ int trend(int* array, int start=currIdx, int count=10){
     i=incmod(i, count);
   } while (i!=start); 
   
+  return balance;
   if (balance>0) return 1; else if (balance<0) return -1; else return 0;
 }
 int thmVals[2] = {0,0};
-void readThm(int count=10){
+void readThm(int count=60){
   int thm=0;
   for (int i=0; i<100; i++){
     thm += (analogRead(thm_int_pin)/1024.0)*5000;
@@ -54,14 +55,10 @@ void readThm(int count=10){
   thmVals[0]=thm;
   thm1_history[currIdx]=thm/5;
   
-  thm=0;
-  for (int i=0; i<100; i++){
-    thm += (analogRead(thm_ext_pin)/1024.0)*5000;
-  }
-  thm /= 100;
-  thm /= 2; thm *= 2;
-  thmVals[1]=thm;
-  thm1_history[currIdx]=thm/5;
+  sensors.requestTemperatures(); 
+  thm = sensors.getTempCByIndex(0)*10;
+  thmVals[1] = thm;
+  thm2_history[currIdx]=thm/2;
   
   
   currIdx=incmod(currIdx, count);
@@ -71,18 +68,28 @@ int currentMode = 1;
 int secondsInCurrentMode = -1;
 const int modeCound=2;
 void displaySecondLine(){
+  if (secondsInCurrentMode<0){
+   if (now.second()%10 != 9) return; //align thm zone changes to x0 and x5 secs
+  }
   secondsInCurrentMode++;
   secondsInCurrentMode%=5;
-  if (secondsInCurrentMode!=0) return;
+  if (secondsInCurrentMode!=0) {
+   return; 
+  }
+  else {
+    currentMode++;
+    currentMode%=2;
+  }
   
   readThm();
   
-  int thm = thmVals[1];
+  int thm = thmVals[currentMode];
   
   lcd.setCursor(0,1); 
   
-  lcd.write(byte(3));
-  int curr_trend = trend(thm1_history);
+  if (currentMode==0) lcd.write(byte(3)); else lcd.write(byte(4));
+  int curr_trend;
+  if (currentMode==0) curr_trend=trend(thm1_history); else curr_trend=trend(thm2_history);
   if (curr_trend>0){
     lcd.write(byte(0));
   }
@@ -95,6 +102,6 @@ void displaySecondLine(){
   lcd.print(" ");
   if (thm<10) lcd.print("0"); 
   lcd.print(thm/10);lcd.print(".");lcd.print(thm%10);
-  lcd.write(byte(2));lcd.print(" ");
+  lcd.write(byte(2));lcd.print("                        ");
   
 }
